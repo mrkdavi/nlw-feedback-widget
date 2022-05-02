d978a506cfa1f4ae49c6c71fd325514db491c61c
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-02 13:26:27 -0300

    Criando o Widget Button com Tailwind e Headless UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget";
 
-function App() {
-  return <h1>Hello World</h1>
+export function App() {
+  return <Widget />
 }
-
-export default App
