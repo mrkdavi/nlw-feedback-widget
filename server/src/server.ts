bec8c455f6981affab75c515e6a63ab4af606d3f
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-05-08 09:27:15 -0300

    Atualizando dependencias

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 const port = 3333;
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World! ')
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`HTTP server running at port ${port}...`)
